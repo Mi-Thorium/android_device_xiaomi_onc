2dd8bb4ed65cc16e0b12420fa83dc682d5e14158
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-22 21:04:43 +0800

    onc: Add AOSP product makefile

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_onc-user \
     lineage_onc-userdebug \
     lineage_onc-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/aosp_onc.mk
+
+COMMON_LUNCH_CHOICES += \
+    aosp_onc-user \
+    aosp_onc-userdebug \
+    aosp_onc-eng
---
+++ aosp_onc.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from onc device
+$(call inherit-product, device/xiaomi/onc/device.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := onc
+PRODUCT_NAME := aosp_onc
+BOARD_VENDOR := xiaomi
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := Redmi 7
+PRODUCT_MANUFACTURER := xiaomi
+TARGET_VENDOR := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="onc-user 10 QKQ1.191008.001 V12.5.3.0.QFLCNXM release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "xiaomi/onc/onc:10/QKQ1.191008.001/V12.5.3.0.QFLCNXM:user/release-keys"
