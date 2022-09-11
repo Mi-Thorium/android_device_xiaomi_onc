a1a8f32aebc27f3196303492c29c30b80bc38dd3
me-cafebabe <me_cafebabe@hotmail.com>
2022-09-11 15:58:04 +0800

    onc: camera: Temporarily disable OSS Camera HAL
    TODO: Re-enable it when oss camera halbringup is ready

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,3 +1,5 @@
+ifeq (0,1)
+
 MM_V4L2_DRIVER_LIST += msm8960
 MM_V4L2_DRIVER_LIST += msm8974
 MM_V4L2_DRIVER_LIST += msm8916
@@ -26,4 +28,6 @@ ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
       include $(call all-subdir-makefiles)
     endif
   endif
+endif
+
 endif
